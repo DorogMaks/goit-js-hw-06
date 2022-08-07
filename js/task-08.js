1feb5c20cb7e43bacf6e4a53d2c9204f579c3f34
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-07 22:12:45 +0300

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+const handleSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('All fields must be filled!');
+  }
+
+  const userDetails = { Email: email.value, Password: password.value };
+
+  console.log(userDetails);
+
+  event.currentTarget.reset();
+};
+
+form.addEventListener('submit', handleSubmit);
