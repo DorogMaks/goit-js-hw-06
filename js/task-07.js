cb9fe1c482e9b99d48b1c58e636626e3b0ecc6de
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-08 16:11:50 +0300

    add task-09

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
-inputEl.addEventListener('input', event => {
+inputEl.addEventListener('input', () => {
   textEl.style.fontSize = `${inputEl.value}px`;
 });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const magicBtn = document.querySelector('.change-color');
+const bgColorValue = document.querySelector('.color');
+
+magicBtn.addEventListener('click', () => {
+  let colorValue = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorValue;
+  bgColorValue.textContent = colorValue;
+});
