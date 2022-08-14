e8d10d6157aaf1a18c52c9ec284530b9785cfe89
Maksym Dorohokuplya <dorogmaks@gmail.com>
2022-08-14 18:37:28 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,30 @@
-const counterValue = document.querySelector('#value');
-counterValue.textContent = 0;
+let counterValue = 0;
 
+const counterDisplayValue = document.querySelector('#value');
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 
-const decrementClick = () => (counterValue.textContent -= 1);
-const incrementClick = () =>
-  (counterValue.textContent = Number(counterValue.textContent) + 1);
-
 decrementBtn.addEventListener('click', decrementClick);
 incrementBtn.addEventListener('click', incrementClick);
+
+function decrementClick() {
+  counterValue -= 1;
+  counterDisplayValue.textContent = counterValue;
+}
+function incrementClick() {
+  counterValue += 1;
+  counterDisplayValue.textContent = counterValue;
+}
+
+// const counterValue = document.querySelector('#value');
+// counterValue.textContent = 0;
+
+// const decrementBtn = document.querySelector('button[data-action="decrement"]');
+// const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+// const decrementClick = () => (counterValue.textContent -= 1);
+// const incrementClick = () =>
+//   (counterValue.textContent = Number(counterValue.textContent) + 1);
+
+// decrementBtn.addEventListener('click', decrementClick);
+// incrementBtn.addEventListener('click', incrementClick);
